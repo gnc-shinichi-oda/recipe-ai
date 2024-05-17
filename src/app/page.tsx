3e016e454d248c702ab615a7475f6e15d8cd6cb7
gnc-shinichi-oda <104481614+gnc-shinichi-oda@users.noreply.github.com>
2024-05-17 15:03:27 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             type="submit"
             className="  text-white p-2 rounded-lg bg-blue-500   w-1/2 text-xl  "
           >
-            レシピ生成
+            Generate
           </button>
         </form>
       </section>
